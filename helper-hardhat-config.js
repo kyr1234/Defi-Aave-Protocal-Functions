4cbc6031cb104361924ee122b01e8bdf111be0d5
kyr1234 <yugkhokhar18@gmail.com>
2022-07-26 09:58:23 +0530

    completed

---
+++ helper-hardhat-config.js
@@ -0,0 +1,26 @@
+const networkConfig = {
+    31337: {
+        name: "localhost",
+        wethToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        lendingPoolAddressesProvider: "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+        daiEthPriceFeed: "0x773616E4d11A78F511299002da57A0a94577F1f4",
+        daiToken: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+    },
+    // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
+    // Default one is ETH/USD contract on Kovan
+    42: {
+        name: "kovan",
+        ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+        wethToken: "0xd0A1E359811322d97991E03f863a0C30C2cF029C",
+        lendingPoolAddressesProvider: "0x88757f2f99175387aB4C6a4b3067c77A695b0349",
+        daiEthPriceFeed: "0x22B58f1EbEDfCA50feF632bD73368b2FdA96D541",
+        daiToken: "0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
